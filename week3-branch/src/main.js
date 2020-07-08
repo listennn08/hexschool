3339b015fc2129ffdad271264b62bc53991b9545
listennn <listennn@Listens-MacBook-Pro.local>
2020-07-08 23:56:56 +0800

    add ESLint

--- week3-branch/src/main.js
+++ week3-branch/src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import lodash from 'lodash'
-import VueLodash from 'vue-lodash'
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import lodash from 'lodash';
+import VueLodash from 'vue-lodash';
+import App from './App.vue';
 
 Vue.use(VueAxios, axios);
-Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
-Vue.config.productionTip = false
+Vue.use(VueLodash, { name: 'custom', lodash: lodash });
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount('#app');
