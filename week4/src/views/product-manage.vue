f09360c063167e482ba76a104495341f167af6aa
listennn <listennn@Listens-MacBook-Pro.local>
2020-07-17 23:55:08 +0800

    fix bug

--- week4/src/views/product-manage.vue
+++ week4/src/views/product-manage.vue
@@ -194,6 +194,8 @@ export default {
                                     },
                                 });
                             });
+                    } else {
+                        loader.hide();
                     }
                 },
             });
