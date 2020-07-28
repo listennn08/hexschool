eaf51a342058e6fe11cb69f32efcfc147227c433
MattTai <listennn08@outlook.com>
2020-07-28 17:18:18 +0800

    toLF

--- week6/vue.config.js
+++ week6/vue.config.js
@@ -1,11 +1,11 @@
 // vue.config.js
-const path = require("path");
-const resolve = dir => path.join(__dirname, dir);
-const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
+// const path = require('path');
+// const resolve = (dir) => path.join(__dirname, dir);
+// const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
 
 module.exports = {
   // options...
   publicPath: process.env.NODE_ENV === 'production'
     ? '/hexschool/week6/'
     : '/',
-}
+};
