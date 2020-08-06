3ce622ade025009f18a006867a4f4e5e9b32c7db
MattTai <listennn08@outlook.com>
2020-08-06 16:32:15 +0800

    update

--- week7/src/main.js
+++ week7/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/main.sass';
-import './plugins/Loading';
+import './plugins/loading';
 import './plugins/confirm';
 import './plugins/fontawesome';
 import './plugins/vee-validate';
