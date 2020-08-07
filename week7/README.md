b26534aa2e548f699f31505e0cf0f3e1e254a40b
MattTai <listennn08@outlook.com>
2020-08-07 13:26:16 +0800

    remove week7

--- week7/README.md
+++
@@ -1,1 +0,0 @@
-demo: https://listennn08.github.io/hexschool/week7/
--- week7/vue.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // options...
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/hexschool/week7/'
-    : '/',
-};
